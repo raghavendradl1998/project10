4bcfd2a3d12d72351b2013844f751ff3fc74fa9a
raghavendra.dl <raghavendra.dl@ciphercode.co>
2023-03-19 13:27:19 +0530

    portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,54 +1,74 @@
-import React from "react"
-import Back from "../common/back/Back"
-import "./contact.css"
 
+import './contact.css';
+import React, { useRef } from 'react';
+import {AiOutlineMail} from 'react-icons/ai';
+import{FaFacebookMessenger} from 'react-icons/fa';
+import{BsWhatsapp} from 'react-icons/bs';
+
+import emailjs from 'emailjs-com';
 const Contact = () => {
-  const map = 'https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d904726.6131739549!2d85.24565535!3d27.65273865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1652535615693!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_9g359ed', 'template_fyynund', form.current, 'zdbYGXLhtS0xcIwra')
+      
+  e.target.reset()
+  };
+
   return (
-    <>
-      <Back title='Contact us' />
-      <section className='contacts padding'>
-        <div className='container shadow flexSB'>
-          <div className='left row'>
-            <iframe src={map}></iframe>
-          </div>
-          <div className='right row'>
-            <h1>Contact us</h1>
-            <p>We're open for any suggestion or just to have a chat</p>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
 
-            <div className='items grid2'>
-              <div className='box'>
-                <h4>ADDRESS:</h4>
-                <p>198 West 21th Street, Suite 721 New York NY 10016</p>
-              </div>
-              <div className='box'>
-                <h4>EMAIL:</h4>
-                <p> info@yoursite.com</p>
-              </div>
-              <div className='box'>
-                <h4>PHONE:</h4>
-                <p> + 1235 2355 98</p>
-              </div>
-            </div>
+      <div className="container contact__container">
+        <div className="contact_options">
+          <article className='contact__option'>
 
-            <form action=''>
-              <div className='flexSB'>
-                <input type='text' placeholder='Name' />
-                <input type='email' placeholder='Email' />
-              </div>
-              <input type='text' placeholder='Subject' />
-              <textarea cols='30' rows='10'>
-                Create a message here...
-              </textarea>
-              <button className='primary-btn'>SEND MESSAGE</button>
-            </form>
+            <AiOutlineMail  className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>raghavendradl1998@gmail.com</h5>
+            <a href="mailto:raghavendradl1998@gmail.com">Send a message</a>
+            
+          </article>
 
-            <h3>Follow us here</h3>
-            <span>FACEBOOK TWITTER INSTAGRAM DRIBBBLE</span>
-          </div>
+
+
+          <article className='contact__option'>
+
+            <FaFacebookMessenger   className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>message </h5>
+            <a href="#">Send a message</a>
+            
+          </article>
+
+
+
+          <article className='contact__option'>
+
+            <BsWhatsapp   className='contact__option-icon'/>
+            <h4>WhatsApp</h4>
+            <h5>8050362712</h5>
+            <a href="https://api.whatsapp.com/send?phone+=8050362712">Send a message</a>
+            
+          </article>
         </div>
-      </section>
-    </>
+      {/* END OF THE FORM */}
+        <form ref={form} onSubmit = {sendEmail}>
+          <input type="text" name="name" placeholder='Your Full Name' required/>
+          <input type="email" name="email" placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your message' required/>
+          <button type="submit" className='btn btn-primary'>Send Message</button>
+
+        </form>
+      </div>
+
+
+
+
+    </section>
   )
 }
 
